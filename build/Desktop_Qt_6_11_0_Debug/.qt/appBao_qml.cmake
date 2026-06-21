78e3a87683cb6277a18345c9e9d28f1af37d6db5
CassimJT <cassimjaffer47@gmail.com>
2026-06-21 09:39:20 +0200

    Initial commit

---
+++ build/Desktop_Qt_6_11_0_Debug/.qt/appBao_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appBao")
+set(working_dir "/home/csociety/CISociety/Qt/Projects/Bao")
+set(src_and_dest_list
+    "/home/csociety/CISociety/Qt/Projects/Bao/Main.qml"
+    "/home/csociety/CISociety/Qt/Projects/Bao/build/Desktop_Qt_6_11_0_Debug/Bao/Main.qml"
+
+)
+set(timestamp_file "/home/csociety/CISociety/Qt/Projects/Bao/build/Desktop_Qt_6_11_0_Debug/.qt/appBao_qml.txt")
